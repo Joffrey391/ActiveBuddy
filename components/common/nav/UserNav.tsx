64d215b9255c17f1f9688aae3cc0ec12143eeaf7
Joffrey391 <michal2314534@gmail.com>
2023-12-23 13:53:01 +0100

    User model

--- components/common/nav/UserNav.tsx
+++ components/common/nav/UserNav.tsx
@@ -6,13 +6,25 @@ import { HiLightBulb } from 'react-icons/hi'
 import { GitHubAuthButton } from '@/components/button';
 import ProfileHead from '../ProfileHead';
 import DropdownOptions, { dropdownOptions } from '../DropdownOptions';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 interface Props { }
 
 const UserNav: FC<Props> = (props): JSX.Element => {
+    const { data, status } = useSession();
+    const isAuth = status === 'authenticated'
+    const handleLoginWithGithub = async () => {
+        const res = await signIn('github');
+        console.log(res)
+    }
+
     const dropdownOptions: dropdownOptions = [
         { label: 'Dashboard', onClick() { } },
-        { label: 'Logout', onClick() { } },
+        {
+            label: 'Logout', async onClick() {
+                await signOut()
+            }
+        },
     ]
 
     return <div className='flex items-center justify-between bg-primary-dark p-3'>
@@ -27,8 +39,14 @@ const UserNav: FC<Props> = (props): JSX.Element => {
                 <HiLightBulb size={34} />
             </button>
 
-            <GitHubAuthButton lightOnly />
-            <DropdownOptions options={dropdownOptions} head={<ProfileHead nameInitial='M' lightOnly />} />
+            {isAuth ? (
+                <DropdownOptions
+                    options={dropdownOptions}
+                    head={<ProfileHead nameInitial='M' lightOnly />}
+                />
+            ) : (
+                <GitHubAuthButton onClick={handleLoginWithGithub} lightOnly />
+            )}
         </div>
     </div>;
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
+import DefaultLayout from '@/components/layout/DefaultLayout'
 import '@/styles/globals.css'
+import { Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface Props {
+    session?: Session | null
+}
+
+export default function App({ Component, pageProps }: AppProps<Props>) {
+    return (
+        <SessionProvider session={pageProps.session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+    )
+
 }
